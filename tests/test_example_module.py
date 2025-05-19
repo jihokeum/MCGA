801ee85aea578cb0d8d0459f050ccc53eaa80c0f
jihokeum <jiho.keum@epfl.ch>
2025-05-19 16:44:13 +0200

    modified tests files

--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -1,6 +1,5 @@
-from mcga.example_module import hello_smiles
-
-
-# Test the function
-def test_hello_smiles():
-    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
+def hello_smiles():
+    """
+    A simple example function for tests.
+    """
+    return "🙂"
